e3c9d6430ea74595c423e4df2810828e801d36d0
Jack Seery <Zebradog09@gmail.com>
2020-05-02 21:08:00 -0700

    Made interactable buttons
    Did a bunch of other stuff too, made a second scene and added some assets for bowling pins

---
+++ Assets/SceneSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void HubScene()
+    {
+        SceneManager.LoadScene("samplescene");
+    }
+
+    public void BowlingScene()
+    {
+        SceneManager.LoadScene("Advanced_Bowling_Demo");
+    }
+}
