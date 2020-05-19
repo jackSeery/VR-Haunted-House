9947db7116e5993650c19144443eddb90d7eb32e
JasonGuo <yuzhe.guo@bellevuecollege.edu>
2020-05-18 20:47:40 -0700

    Added a rotating sphere
    Made a sphere move in a circle

---
+++ Assets/GalaxyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalaxyMovement : MonoBehaviour
+{
+    float timeCounter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime;
+
+        float x = Mathf.Cos (timeCounter);
+        float y = Mathf.Sin (timeCounter);
+        float z = 0;
+
+        transform.position = new Vector3 (x, y, z);
+    }
+}
