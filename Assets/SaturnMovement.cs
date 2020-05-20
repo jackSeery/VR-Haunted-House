cccf8b0f9561b9302ed9fecedadf234b7df7a5df
JasonGuo <yuzhe.guo@bellevuecollege.edu>
2020-05-20 11:01:39 -0700

    Eight Planets Spin Around Sun
    Eight planets spin around Sun with the same speed

---
+++ Assets/SaturnMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaturnMovement : MonoBehaviour
+{
+    float timeCounter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime;
+
+        float x = 40 * Mathf.Cos (timeCounter);
+        float y = 0;
+        float z = 40 * Mathf.Sin (timeCounter);
+
+        transform.position = new Vector3 (x, y, z);
+    }
+}
