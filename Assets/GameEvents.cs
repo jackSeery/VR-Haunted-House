c40685fd4449f22b16f51033e90cfeabcc46c9ef
Jack Seery <Zebradog09@gmail.com>
2020-05-22 15:33:53 -0700

    Added more bowling pins, added Event object

---
+++ Assets/GameEvents.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+
+    public static int score = 0;
+    public static GameEvents current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public event Action onPinFall;
+    public void PinFall()
+    {
+        if(onPinFall != null)
+        {
+            onPinFall();
+        }
+    }
+}
---
+++ Assets/PinController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinController : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameEvents.current.onPinFall += OnPinFall;
+    }
+
+    private void OnPinFall()
+    {
+        GameEvents.score++;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (this.gameObject.transform.rotation.x > 30 || this.gameObject.transform.rotation.z > 30)
+        {
+            GameEvents.current.PinFall();
+            Destroy(this.gameObject);
+        }
+    }
+
+}
