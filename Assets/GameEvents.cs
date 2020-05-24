6d92f6b413303bfe0f3afa771f2a5fde2a8c39e5
Jack Seery <Zebradog09@gmail.com>
2020-05-24 01:52:15 -0700

    Working on game event scripts

--- Assets/GameEvents.cs
+++ Assets/GameEvents.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Valve.Newtonsoft.Json;
 
 public class GameEvents : MonoBehaviour
 {
 
     public static int score = 0;
     public static GameEvents current;
+    public static IObserver<Type> observer;
 
     private void Awake()
     {
+        Debug.Log("Script GameEvents.cs is Awake");
         current = this;
     }
 
--- Assets/PinController.cs
+++ Assets/PinController.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class PinController : MonoBehaviour
 {
-
+    public int id;
+    private Vector3 pinDirection;
+    private Vector3 fallenAngle = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(pinDirection.ToString());
         GameEvents.current.onPinFall += OnPinFall;
+    }
+
+    private void Update()
+    {
+        pinDirection = this.transform.localEulerAngles;
+        Debug.Log("Pin " + this.id + " direction: " + pinDirection.ToString());
+
+        Debug.Log("Angle between upright and Pin " + this.id + " direction: " + Vector3.Angle(fallenAngle, pinDirection));
+
+        if (PinFallenOver(id))
+        {
+            Debug.Log("This Pin object is fallen over: " + this.id);
+        }
     }
 
     private void OnPinFall()
@@ -24,6 +40,11 @@ public class PinController : MonoBehaviour
             GameEvents.current.PinFall();
             Destroy(this.gameObject);
         }
+    }
+
+    private bool PinFallenOver(int id)
+    {
+        return Vector3.Angle(fallenAngle, pinDirection) > 10.0f;
     }
 
 }
